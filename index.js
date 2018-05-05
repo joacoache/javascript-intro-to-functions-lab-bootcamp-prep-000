344190772db716ea2288ac691a72d455b0f9132a
Joaquín Herrera <herrerab.joaquin@gmail.com>
2018-05-05 00:40:18 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,10 +10,12 @@ console.log(shout(string));
 console.log(whisper(string));
 
 function sayHiToGrandma(string){
-  if(string === string.toLowerCase()){
-    return "I can't hear you!"};
-  else if(string === string.toUpperCase()) {
-    return "YES INDEED!"};
-  else if(string === "I love you, Grandma"){
-    return "I love you, too."};
+  if (string === string.toLowerCase()){
+    return "I can't hear you!"
+  } else if (string === string.toUpperCase()) {
+    return "YES INDEED!"
+  } else if (string === "I love you, Grandma"){
+    return "I love you, too."
+  }
 }
+
